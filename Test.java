d2e641b54d1946e3f69d598895791a31b51b2f50
chaoyang1996 <35752540+chaoyang1996@users.noreply.github.com>
2019-10-07 21:19:33 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+public class Test
+{
+	
+	public static void main(String[] args) 
+	{
+		 ArrayList list0 = new ArrayList<Integer>();
+		 long start0 = System.currentTimeMillis();
+		 for(int i =0;i<1000000;i++) {
+			 list0.add(i);
+		 }
+    System.out.println(System.currentTimeMillis()-start0);
+    
+    long start1 = System.currentTimeMillis();
+    ArrayList<Integer> list1 = new ArrayList<Integer>();
+    for(int i = 1000000;i<2000000;i++) {
+    	list1.add(i);
+    }
+    System.out.println(System.currentTimeMillis()-start1);
+}
+	
+	
+}
